dbe54f3cbb837d1ce524106097f118976e11c5b2
Nvlio <jgabriell2009@gmail.com>
2023-05-10 23:50:21 -0300

    completando as interfaces basicas de cada pag

--- umbrellapag/src/Pages/default.jsx
+++ umbrellapag/src/Pages/default.jsx
@@ -1,9 +1,33 @@
+import React from "react"
+import { LinkContainer } from "react-router-bootstrap";
+import "../Style.css"
+
+
 export default function Pagina (props){
+
+    const children = React.Children.toArray(props.children);
+    const img = children.find((child)=>{return child.type==="img"});
+
+    if (img){
+
+        return( 
+            
+            <div>
+            {props.title==="" ? null:<h1>{props.title}</h1>}
+            <div>{img}</div>
+            <hr></hr>
+            <LinkContainer to="/"><h4 className="linkurl" style={{textAlign:"center"}}>{props.desc}</h4></LinkContainer>
+           
+
+        </div>
+        )
+    }else{
     return(
         <div>
             <h1>{props.title}</h1><br/>
-            <h2>{props.desc}</h2>
+            <h4>{props.desc}</h4>
+            <hr></hr>
             <div>{props.children}</div>
         </div>
     )
-} 
+} };
--- umbrellapag/src/pag.jsx
+++ umbrellapag/src/pag.jsx
@@ -4,8 +4,8 @@ import Pagina from "./Pages/default.jsx"
 export function PagHome(){
     return(
         <div><Navibar/>
-        <Pagina title="Home slaa" desc="Bem vindo a umbrella pharmaceutical group.">
-            <h1>SalHome</h1>
+        <Pagina title="" desc="Umbrella corporations, é focada na pesquisa de medicamentos para melhorar a qualidade da sua vida e a de seus entes queridos">
+            <img src={require("./foto/UmbrMHlogo.png")}width={'100%'} height={"100%"} alt="Propaganda da umbrella com uma familia feliz"></img>
         </Pagina>
         </div>
     )
@@ -13,13 +13,23 @@ export function PagHome(){
 
 export function PagProd(){
     return(
-        <div><Navibar/><h1>slaProd</h1></div>
+        <div>
+            <Navibar/>
+            <Pagina title="Nossos produtos" desc="Aqui um catalogo com todos os produtos vendidos por nós para melhorar a qualidade e longevidade da sua vida">
+                {/*aqui vai ser referenciado a pag que vai ter a lista de produtos*/}
+            </Pagina>
+        </div>
     )
 };
 
 export function PagAbout(){
     return(
-        <div><Navibar/><h1>slaAbout</h1></div>
+        <div>
+            <Navibar/>
+            <Pagina title="Sobre nós" desc="A nossa historia até agora">
+                {/*Aqui vai ficar a pag com lista e edescrição dos criadores da umbrella*/}
+            </Pagina>
+        </div>
     )
 };
 
@@ -27,19 +37,30 @@ export function PagJobs(){
     return(
         <div>
             <Navibar/>
-            
+            <Pagina title="Vagas" desc="A umbrella sempre acolhe as mentes mais brilhantes para trabalhar com a gente, veja as vagas disponiveis"></Pagina>
         </div>
     )
 };
 
 export function PagCont(){
     return(
-        <div><Navibar/><h1>slaContact</h1></div>
+        <div>
+            <Navibar/>
+            <Pagina title="Contato" desc="Nossos contatos">
+                
+            </Pagina>    
+        </div>
     )
 };
 
 export function Pag404(){
     return(
-        <div><Navibar/><h1>sla404</h1></div>
+        <div>
+            <Navibar/>
+            <Pagina class="linkurl" title="Infelizmente não encontramos essa pagina" desc="Clique aqui para retornar até a pagina inicial">
+                <img src={require('./foto/Error404.png')} alt="erro404"/>
+                {/*Botão que faz ir para home*/}
+            </Pagina>
+        </div>
     )
 };
